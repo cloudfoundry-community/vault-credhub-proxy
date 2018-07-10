6ee6f027433ec724717443bbce564b19643c3069
Ruben Koster <rkoster@starkandwayne.com>
2018-07-10 16:04:15 +0200

    Finish REST api implementation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,11 +10,11 @@ import (
 
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub"
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/auth"
+	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials/values"
 	"github.com/gorilla/mux"
 	"github.com/qntfy/kazaam"
 )
 
-// our main function
 func main() {
 	ch, err := getCredhubClient()
 	if err != nil {
@@ -23,9 +24,9 @@ func main() {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/v1").Subrouter()
 	subrouter.HandleFunc("/{path:.*}", ListSecret(ch)).Methods("GET").Queries("list", "1")
-	subrouter.HandleFunc("/{path:.*}", GetSecret).Methods("GET")
-	subrouter.HandleFunc("/{path:.*}", SetSecret).Methods("POST")
-	subrouter.HandleFunc("/{path:.*}", DelSecret).Methods("DELETE")
+	subrouter.HandleFunc("/{path:.*}", GetSecret(ch)).Methods("GET")
+	subrouter.HandleFunc("/{path:.*}", SetSecret(ch)).Methods("POST")
+	subrouter.HandleFunc("/{path:.*}", DelSecret(ch)).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
@@ -47,18 +48,65 @@ func ListSecret(ch *credhub.CredHub) func(http.ResponseWriter, *http.Request) {
 	}
 }
 
-func GetSecret(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	log.Printf("get path %s", params["path"])
+func GetSecret(ch *credhub.CredHub) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := mux.Vars(r)["path"]
+		cred, err := ch.GetLatestVersion(path)
+		if err != nil {
+			http.Error(w, "Not Found or Access Denied",
+				http.StatusNotFound)
+		}
+		j, _ := json.Marshal(cred)
+		var k *kazaam.Kazaam
+		switch cred.Metadata.Type {
+		case "password":
+			k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data.password": "value"}}]`, kazaam.NewDefaultConfig())
+		case "value":
+			k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data.value": "value"}}]`, kazaam.NewDefaultConfig())
+		default:
+			k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data": "value"}}]`, kazaam.NewDefaultConfig())
+		}
+		o, _ := k.TransformInPlace(j)
+		w.Write(o)
+
+		log.Printf("get path %s", path)
+	}
 }
 
-func SetSecret(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	log.Printf("set path %s", params["path"])
+func SetSecret(ch *credhub.CredHub) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := mux.Vars(r)["path"]
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body",
+				http.StatusInternalServerError)
+		}
+		value := values.JSON{}
+		err = json.Unmarshal(body, &value)
+		cred, err := ch.SetJSON(path, value, credhub.Converge)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error setting secret: %s", err),
+				http.StatusInternalServerError)
+		}
+
+		j, _ := json.Marshal(cred)
+		k, _ := kazaam.New(`[{"operation": "shift", "spec": {"data": "value"}}]`, kazaam.NewDefaultConfig())
+		o, _ := k.TransformInPlace(j)
+		w.Write(o)
+		log.Printf("set path %s", path)
+	}
 }
-func DelSecret(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	log.Printf("del path %s", params["path"])
+
+func DelSecret(ch *credhub.CredHub) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := mux.Vars(r)["path"]
+		err := ch.Delete(path)
+		if err != nil {
+			http.Error(w, "Error in Delete",
+				http.StatusInternalServerError)
+		}
+		log.Printf("del path %s", path)
+	}
 }
 
 func getCredhubClient() (*credhub.CredHub, error) {
