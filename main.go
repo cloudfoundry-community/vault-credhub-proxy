c0fced28626284953c5afd6fca881b0019a3baaf
Ruben Koster <rkoster@starkandwayne.com>
2018-07-12 10:36:39 +0200

    Add token authentication

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub"
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/auth"
@@ -16,100 +17,111 @@ import (
 )
 
 func main() {
-	ch, err := getCredhubClient()
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/v1").Subrouter()
+	subrouter.HandleFunc("/{path:.*}", ListSecret).Methods("GET").Queries("list", "1")
+	subrouter.HandleFunc("/{path:.*}", GetSecret).Methods("GET")
+	subrouter.HandleFunc("/{path:.*}", SetSecret).Methods("POST")
+	subrouter.HandleFunc("/{path:.*}", DelSecret).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func ListSecret(w http.ResponseWriter, r *http.Request) {
+	ch, err := getCredhubClient(r.Header.Get("X-Vault-Token"))
 	if err != nil {
 		log.Fatal("Error connection to Credhub: ", err)
 	}
 
-	router := mux.NewRouter()
-	subrouter := router.PathPrefix("/v1").Subrouter()
-	subrouter.HandleFunc("/{path:.*}", ListSecret(ch)).Methods("GET").Queries("list", "1")
-	subrouter.HandleFunc("/{path:.*}", GetSecret(ch)).Methods("GET")
-	subrouter.HandleFunc("/{path:.*}", SetSecret(ch)).Methods("POST")
-	subrouter.HandleFunc("/{path:.*}", DelSecret(ch)).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func ListSecret(ch *credhub.CredHub) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		path := mux.Vars(r)["path"]
-		results, err := ch.FindByPath(path)
-		if err != nil {
-			log.Printf("error returned by credhub FindByPath %s", err)
-		}
-
-		j, _ := json.Marshal(results.Credentials)
-		k, _ := kazaam.New(`[{"operation": "shift", "spec": {"data": "$"}}]`, kazaam.NewDefaultConfig())
-		o, _ := k.TransformInPlace(j)
-		k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data.keys": "data[*].name"}}]`, kazaam.NewDefaultConfig())
-		o, _ = k.TransformInPlace(o)
-		w.Write(o)
-		log.Printf("list path %s", path)
+	path := mux.Vars(r)["path"]
+	results, err := ch.FindByPath(path)
+	if err != nil {
+		log.Printf("error returned by credhub FindByPath %s", err)
 	}
+
+	j, _ := json.Marshal(results.Credentials)
+	k, _ := kazaam.New(`[{"operation": "shift", "spec": {"data": "$"}}]`, kazaam.NewDefaultConfig())
+	o, _ := k.TransformInPlace(j)
+	k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data.keys": "data[*].name"}}]`, kazaam.NewDefaultConfig())
+	o, _ = k.TransformInPlace(o)
+	w.Write(o)
+	log.Printf("list path %s", path)
+
 }
 
-func GetSecret(ch *credhub.CredHub) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		path := mux.Vars(r)["path"]
-		cred, err := ch.GetLatestVersion(path)
-		if err != nil {
-			http.Error(w, "Not Found or Access Denied",
-				http.StatusNotFound)
-		}
-		j, _ := json.Marshal(cred)
-		var k *kazaam.Kazaam
-		switch cred.Metadata.Type {
-		case "password":
-			k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data.password": "value"}}]`, kazaam.NewDefaultConfig())
-		case "value":
-			k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data.value": "value"}}]`, kazaam.NewDefaultConfig())
-		default:
-			k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data": "value"}}]`, kazaam.NewDefaultConfig())
-		}
-		o, _ := k.TransformInPlace(j)
-		w.Write(o)
-
-		log.Printf("get path %s", path)
+func GetSecret(w http.ResponseWriter, r *http.Request) {
+	ch, err := getCredhubClient(r.Header.Get("X-Vault-Token"))
+	if err != nil {
+		log.Fatal("Error connection to Credhub: ", err)
 	}
-}
 
-func SetSecret(ch *credhub.CredHub) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		path := mux.Vars(r)["path"]
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Error reading request body",
-				http.StatusInternalServerError)
-		}
-		value := values.JSON{}
-		err = json.Unmarshal(body, &value)
-		cred, err := ch.SetJSON(path, value, credhub.Converge)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error setting secret: %s", err),
-				http.StatusInternalServerError)
-		}
-
-		j, _ := json.Marshal(cred)
-		k, _ := kazaam.New(`[{"operation": "shift", "spec": {"data": "value"}}]`, kazaam.NewDefaultConfig())
-		o, _ := k.TransformInPlace(j)
-		w.Write(o)
-		log.Printf("set path %s", path)
+	path := mux.Vars(r)["path"]
+	cred, err := ch.GetLatestVersion(path)
+	if err != nil {
+		http.Error(w, "Not Found or Access Denied",
+			http.StatusNotFound)
 	}
-}
-
-func DelSecret(ch *credhub.CredHub) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		path := mux.Vars(r)["path"]
-		err := ch.Delete(path)
-		if err != nil {
-			http.Error(w, "Error in Delete",
-				http.StatusInternalServerError)
-		}
-		log.Printf("del path %s", path)
+	j, _ := json.Marshal(cred)
+	var k *kazaam.Kazaam
+	switch cred.Metadata.Type {
+	case "password":
+		k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data.password": "value"}}]`, kazaam.NewDefaultConfig())
+	case "value":
+		k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data.value": "value"}}]`, kazaam.NewDefaultConfig())
+	default:
+		k, _ = kazaam.New(`[{"operation": "shift", "spec": {"data": "value"}}]`, kazaam.NewDefaultConfig())
 	}
+	o, _ := k.TransformInPlace(j)
+	w.Write(o)
+
+	log.Printf("get path %s", path)
+
 }
 
-func getCredhubClient() (*credhub.CredHub, error) {
+func SetSecret(w http.ResponseWriter, r *http.Request) {
+	ch, err := getCredhubClient(r.Header.Get("X-Vault-Token"))
+	if err != nil {
+		log.Fatal("Error connection to Credhub: ", err)
+	}
+
+	path := mux.Vars(r)["path"]
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body",
+			http.StatusInternalServerError)
+	}
+	value := values.JSON{}
+	err = json.Unmarshal(body, &value)
+	cred, err := ch.SetJSON(path, value, credhub.Converge)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error setting secret: %s", err),
+			http.StatusInternalServerError)
+	}
+
+	j, _ := json.Marshal(cred)
+	k, _ := kazaam.New(`[{"operation": "shift", "spec": {"data": "value"}}]`, kazaam.NewDefaultConfig())
+	o, _ := k.TransformInPlace(j)
+	w.Write(o)
+	log.Printf("set path %s", path)
+
+}
+
+func DelSecret(w http.ResponseWriter, r *http.Request) {
+	ch, err := getCredhubClient(r.Header.Get("X-Vault-Token"))
+	if err != nil {
+		log.Fatal("Error connection to Credhub: ", err)
+	}
+
+	path := mux.Vars(r)["path"]
+	err = ch.Delete(path)
+	if err != nil {
+		http.Error(w, "Error in Delete",
+			http.StatusInternalServerError)
+	}
+	log.Printf("del path %s", path)
+}
+
+func getCredhubClient(tokenHeader string) (*credhub.CredHub, error) {
+	token := strings.Split(tokenHeader, ":")
 	caCert, err := ioutil.ReadFile(os.Getenv("CREDHUB_CA_CERT"))
 	if err != nil {
 		return nil, err
@@ -118,6 +130,6 @@ func getCredhubClient() (*credhub.CredHub, error) {
 	return credhub.New(
 		os.Getenv("CREDHUB_SERVER"),
 		credhub.CaCerts(string(caCert)),
-		credhub.Auth(auth.UaaClientCredentials(os.Getenv("CREDHUB_CLIENT"), os.Getenv("CREDHUB_SECRET"))),
+		credhub.Auth(auth.UaaClientCredentials(token[0], token[1])),
 	)
 }
