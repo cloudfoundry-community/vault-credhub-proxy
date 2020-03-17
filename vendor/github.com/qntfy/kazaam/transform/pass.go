b63df8050e0289447c8965e92c9619a59f091de3
ramonskie <ramonmakkelie@gmail.com>
2020-03-17 17:30:44 +0100

    remove vendor as we use gomod

--- vendor/github.com/qntfy/kazaam/transform/pass.go
+++
@@ -1,7 +0,0 @@
-package transform
-
-// Pass performs no manipulation of the passed-in data. It is useful
-// for testing/default behavior.
-func Pass(spec *Config, data []byte) ([]byte, error) {
-	return data, nil
-}
